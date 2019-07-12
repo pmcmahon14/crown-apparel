0ad63de177e6cf80794d6fac1b66b6a86b182ee9
Patrick McMahon <pmcmahon14@gmail.com>
2019-07-11 18:02:16 -0700

    basic menu headings set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Homepage from './Homepage';
 import './App.css';
 
 function App() {
     return (
       <div>
-          Hello World!
+          <Homepage/>
       </div>
     );
 }
