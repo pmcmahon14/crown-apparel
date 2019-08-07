060899b1fa58e1e31afb751a58b59d2cf9dd295a
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-07 12:22:21 -0700

    set up Google auth for Firebase and lifecycle events

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -50,7 +50,7 @@ class SignIn extends React.Component{
                         required
                     />
                     <CustomButton type='submit'>Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Sign In With Google</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>{' '}Sign In With Google{' '}</CustomButton>
                 </form>
             </div>
         )
