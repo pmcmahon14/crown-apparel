5b3b8b66e4d9c1b8e71b7e6da5feea1475bdb9ec
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-14 18:19:53 -0700

    added cart component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import {auth} from '../../firebase/utility';
+import CartIcon from '../cart-icon/cart-icon';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
@@ -29,6 +30,7 @@ const Header = ({currentUser}) => (
                         SIGN IN
                     </Link>
                 )}
+                <CartIcon/>
         </div>
     </div>
 );
