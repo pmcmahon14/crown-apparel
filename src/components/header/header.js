fcd42401dbf7cfaf7b46636aec34785dd1f6a0c6
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-14 18:32:15 -0700

    added cart dropdown component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 
 import {auth} from '../../firebase/utility';
 import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
@@ -32,6 +33,7 @@ const Header = ({currentUser}) => (
                 )}
                 <CartIcon/>
         </div>
+        <CartDropdown/>
     </div>
 );
 
