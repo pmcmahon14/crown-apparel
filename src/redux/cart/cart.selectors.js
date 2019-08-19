4afa4d43e443382e26909de2a0cd507f503ecd65
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-19 14:27:15 -0700

    added headers to checkout page with total price

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,7 +16,18 @@ export const selectCartItemCount = createSelector(
     [selectCartItems],
     cartItems =>
         cartItems.reduce(
-            (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+            (accumulatedQuantity, cartItem) =>
+                accumulatedQuantity + cartItem.quantity,
             0
         )
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+        cartItems.reduce(
+            (accumulatedQuantity, cartItem) =>
+                accumulatedQuantity + cartItem.quantity * cartItem.price,
+            0
+        )
+)
