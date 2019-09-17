ba3b77bce0bd130365a9df6ebe80ac586f7994ac
Patrick McMahon <pmcmahon14@gmail.com>
2019-09-17 16:13:26 -0700

    added collectionRef to App page for Firebase

--- src/App.js
+++ src/App.js
@@ -12,16 +12,17 @@ import CheckoutPage from "./pages/checkout/checkout";
 
 import Header from './components/header/header';
 
-import {auth, createUserProfileDocument} from './firebase/utility';
+import {auth, createUserProfileDocument, addCollectionAndDocuments} from './firebase/utility';
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
+import {selectCollectionsForPreview} from "./redux/shop/shop.selectors";
 
 class App extends React.Component {
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        const {setCurrentUser} = this.props;
+        const {setCurrentUser, collectionsArray} = this.props;
 
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
@@ -35,6 +36,7 @@ class App extends React.Component {
                 });
             }
             setCurrentUser(userAuth);
+            addCollectionAndDocuments('collections', collectionsArray);
         });
     }
 
@@ -64,7 +66,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser
+    currentUser: selectCurrentUser,
+    collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
