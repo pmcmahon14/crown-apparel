c9c22fbc2ff8f58879d7ac5642eae736adeceb72
Patrick McMahon <pmcmahon14@gmail.com>
2019-09-26 16:55:51 -0700

    added shop data to redux

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview';
 import CollectionPage from '../collection/collection';
+
+import {updateCollections} from '../../redux/shop/shop.actions';
 
 import {firestore, convertCollectionsSnapshotToMap} from "../../firebase/utility";
 
@@ -10,10 +13,12 @@ class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+        const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
 
-        collectionRef.onSnapshot(async snapshot => {
-            convertCollectionsSnapshotToMap(snapshot);
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
         })
     }
 
@@ -27,8 +32,8 @@ class ShopPage extends React.Component {
         )};
     }
 
+const mapDispatchtoProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
 
-
-
-
-export default ShopPage;
+export default connect(null, mapDispatchtoProps)(ShopPage);
